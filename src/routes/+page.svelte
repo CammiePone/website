0dfb3d04f312890055ff9f03c0f99e4005bd9f22
CammiePone <inazuma261@gmail.com>
2023-05-08 22:17:01 -0400

    probably not gonna use a panorama anymore

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,15 @@
 <svelte:head>
     <title>Home | Cammie's Corner</title>
 </svelte:head>
+<div>
+	<div class="flex flex-row place-content-center max-w-full overflow-x-hidden cycling-images">
+		<img src="/south.webp" alt="" class="left-image inline-flex" />
+		<img src="/west.webp" alt="" class="middle-image inline-flex" />
+		<img src="/north.webp" alt="" class="right-image inline-flex" />
+	</div>
+
+	<div class="hero-text text-center absolute z-10 top-1/2 left-1/2">
+		<h1 class="text-5xl">Hello, I'm Cammie!</h1>
+		<p class="text-2xl">Indie game designer & developer</p>
+	</div>
+</div>
