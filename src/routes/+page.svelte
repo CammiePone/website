67548fb46e321ff8a8352c590f11c281bf515898
CammiePone <inazuma261@gmail.com>
2023-05-09 20:46:18 -0400

    angy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import Carousel from 'svelte-carousel?client'
 	import { browser } from '$app/environment';
-    import { onMount } from 'svelte';
 
-	let carousel;
-	let imgTags;
 	const carouselImgs = [
 		"/carousel/wizard_tower.webp",
 		"/carousel/icarus.webp",
@@ -12,30 +9,14 @@
 		"/carousel/solar_strike.webp",
 		"/carousel/backpacks.webp"
 	];
-	const update=(index) => {
-		let leftImg = imgTags[index];
-		let midImg = imgTags[(index + 1) % carouselImgs.length];
-		let rightImg = imgTags[(index + 2) % carouselImgs.length];
-
-		leftImg.classList.add("left-image");
-		leftImg.classList.remove("middle-image");
-		midImg.classList.add("middle-image");
-		midImg.classList.remove("right-image");
-		rightImg.classList.add("right-image");
-		rightImg.classList.remove("left-image");
-	};
-
-	onMount(async() => {
-		update(carousel.currentPageIndex);
-	});
 </script>
 
 <svelte:head>
     <title>Home | Cammie's Corner</title>
 </svelte:head>
 
-<div id="hero-images" class="relative">
-	<div class="pt-6 pb-6 bg-slate-900 flex flex-row place-content-center max-w-full overflow-x-hidden">
+<div id="hero-images" class="relative overflow-x-hidden">
+	<div class="pt-6 pb-6 bg-slate-900 flex flex-row place-content-center min-w-[150vw] -translate-x-[25vw]">
 		{#if browser}
 			<Carousel
 				particlesToShow={3}
@@ -44,16 +25,21 @@
 				arrows={false}
 				swiping={false}
 				pauseOnFocus={true}
-				bind:this={carousel}
-				on:pageChange={
-					event => {
-						let index = event.detail;
-						update(index);
-					}
-				}
+				let:currentPageIndex
 			>
-				{#each carouselImgs as src, i}
-					<img {src} alt="" class="middle-image brightness-50 rounded-2xl" bind:this={imgTags[i].ref} height="500" width="888">
+				{#each carouselImgs as src, index(src)}
+					<div class="px-4">
+						{#if index === currentPageIndex}
+							<img {src} alt="" class="left-image brightness-50 rounded-2xl" height="500" width="888">
+						{:else if index === (currentPageIndex + 1) % carouselImgs.length}
+							<img {src} alt="" class="middle-image brightness-50 rounded-2xl" height="500" width="888">
+						{:else if index === (currentPageIndex + 2) % carouselImgs.length}
+							<img {src} alt="" class="right-image brightness-50 rounded-2xl" height="500" width="888">
+						{:else}
+							<img {src} alt="" class="brightness-50 rounded-2xl" height="500" width="888">
+						{/if}
+					</div>
+					<!-- <img {src} alt="" class="middle-image brightness-50 rounded-2xl" bind:this={imgTags[i].ref} height="500" width="888"> -->
 				{/each}
 			</Carousel>
 		{/if}
@@ -72,14 +58,14 @@
 <div class="text-darktext text-justify text-xl">
 	<div class="bg-lighttext mr-[10vw] my-[4vh] py-4 px-6 rounded-e-3xl flex flex-row place-content-start items-center">
 		<img src="/wizard_hat.webp" alt="" height="128" width="128" class="mx-6">
-		<p>
+		<p class="max-w-4xl m-auto">
 			I've been working as an indie game designer and developer since September 2017. I got my start by learning Java and making mods for <em>Minecraft: Java Edition</em>.
 			<br>
 			Since then, I've branched out to making my own demos in different game engines, including Godot, Unity3D, and Unreal Engine 4. I've picked up several other programming languages, including C#, JavaScript, TypeScript, C++, Python, Rust, and more!
 		</p>
 	</div>
 	<div class="bg-lighttext ml-[10vw] my-[4vh] py-4 px-6 rounded-s-3xl flex flex-row place-content-end items-center">
-		<p>
+		<p class="max-w-4xl m-auto">
 			Ultimately, game design is what I'm most passionate about. I picked up programming as a means to bring my designs to life, and my design skills are what I'm most proud of. I've honed them to near-perfection over the years, and I'm more than happy to test them against unfamiliar circumstances!
 			<br>
 			As of March 2023, I've finally taken the huge step to start development on my first full game, alongside my partner!
@@ -88,7 +74,7 @@
 	</div>
 	<div class="bg-lighttext mr-[10vw] my-[4vh] py-4 px-6 rounded-e-3xl flex flex-row place-content-start items-center">
 		<img src="/space_helmet.webp" alt="" height="128" width="128" class="mx-6">
-		<p>
+		<p class="max-w-4xl m-auto">
 			I hold myself to very high standards, and know that my work benefits from that immensely. Any project I set my sights on gets that same quality treatment, regardless of if it's my own creation or one I was hired to work on.
 			<br>
 			Despite my humble beginnings, my ambitions are aimed at the stars and my abilities can carry them there!
