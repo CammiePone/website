d95d8de1d2654dbfdcc2ca7b54e68d3b18a1a60c
CammiePone <inazuma261@gmail.com>
2023-05-21 00:01:54 -0400

    started on responsive stuff

--- src/components/UnderConstruction.svelte
+++ src/components/UnderConstruction.svelte
@@ -1,5 +1,5 @@
-<img class="absolute bottom-0 left-8 z-10 h-[70vh]" src="/crane.png" alt="Attribution to Nikita Golubev https://www.flaticon.com/free-icon/crane_1833115">
+<img class="absolute bottom-0 left-8 z-10 md:h-[70vh] h-[40vh]" src="/crane.png" alt="Attribution to Nikita Golubev https://www.flaticon.com/free-icon/crane_1833115">
 
-<div class="under-construction text-darktext translate-y-[25vh] text-right relative text-8xl h-[28vh]">
-	<p class="translate-y-10 p-4 pr-24 bg-accent">Under Construction</p>
+<div class="under-construction text-darktext translate-y-[25vh] text-right relative lg:text-8xl text-4xl lg:h-[28vh] h-[16vh]">
+	<p class="translate-y-10 p-4 md:pr-24 bg-accent">Under Construction</p>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,8 @@
     <title>Home | Cammie's Corner</title>
 </svelte:head>
 
-<div id="hero-images" class="relative overflow-x-hidden min-h-[500px]">
-	<div class="pt-6 pb-6 bg-slate-900 flex flex-row place-content-center min-w-[150vw] -translate-x-[25vw]">
+<div id="hero-images" class="relative overflow-x-hidden h-1/2">
+	<div class="py-[2.5vh] bg-slate-900 flex flex-row place-content-center min-w-[150vw] -translate-x-[25vw]">
 		{#if browser}
 			<Carousel
 				particlesToShow={3}
@@ -24,6 +24,7 @@
 				autoplayDuration={4000}
 				arrows={false}
 				swiping={false}
+				dots={false}
 				pauseOnFocus={true}
 				let:currentPageIndex
 			>
@@ -48,32 +49,32 @@
 		<img src={carouselImgs[index]} alt="" class="middle-image brightness-50 rounded-2xl" height="500" width="888">
 		<img src={carouselImgs[(index + 1) % carouselImgs.length]} alt="" class="right-image brightness-50 rounded-2xl" height="500" width="888"> -->
 	</div>
-	<div class="hero-text text-center absolute top-1/2 left-1/2 z-10">
-		<h1 class="text-5xl">Hello, I'm Cammie!</h1>
-		<p class="text-sm [text-shadow:_none]">Fae / Faer</p>
-		<p class="text-2xl mt-4">Indie Game Designer & Developer</p>
+	<div class="hero-text text-center absolute top-1/2 left-1/2 z-10 w-full">
+		<h1 class="md:text-5xl text-3xl">Hello, I'm Cammie!</h1>
+		<p class="md:text-sm text-xs [text-shadow:_none]">Fae / Faer</p>
+		<p class="md:text-2xl mt-4">Indie Game Designer & Developer</p>
 	</div>
 </div>
 
 <div class="text-darktext text-justify text-xl">
-	<div class="bg-lighttext mr-[10vw] my-[4vh] py-4 px-6 rounded-e-3xl flex flex-row place-content-start items-center">
-		<img src="/wizard_hat.webp" alt="" height="128" width="128" class="mx-6">
+	<div class="bg-lighttext mr-[10vw] my-[4vh] py-4 px-6 rounded-e-3xl flex md:flex-row flex-col place-content-start items-center">
+		<img src="/wizard_hat.webp" alt="" height="128" width="128" class="md:mx-6 md:my-0 mb-6">
 		<p class="max-w-4xl m-auto">
 			I've been working as an indie game designer and developer since September 2017. I got my start by learning Java and making mods for <em>Minecraft: Java Edition</em>.
 			<br>
 			Since then, I've branched out to making my own demos in different game engines, including Godot, Unity3D, and Unreal Engine 4. I've picked up several other programming languages, including C#, JavaScript, TypeScript, C++, Python, Rust, and more!
 		</p>
 	</div>
-	<div class="bg-lighttext ml-[10vw] my-[4vh] py-4 px-6 rounded-s-3xl flex flex-row place-content-end items-center">
+	<div class="bg-lighttext ml-[10vw] my-[4vh] py-4 px-6 rounded-s-3xl flex md:flex-row flex-col place-content-end items-center">
 		<p class="max-w-4xl m-auto">
 			Ultimately, game design is what I'm most passionate about. I picked up programming as a means to bring my designs to life, and my design skills are what I'm most proud of. I've honed them to near-perfection over the years, and I'm more than happy to test them against unfamiliar circumstances!
 			<br>
 			As of March 2023, I've finally taken the huge step to start development on my first full game, alongside my partner!
 		</p>
-		<img src="/end_fable_logo.webp" alt="" height="128" width="128" class="mx-6">
+		<img src="/end_fable_logo.webp" alt="" height="128" width="128" class="md:mx-6 md:my-0 mt-6">
 	</div>
-	<div class="bg-lighttext mr-[10vw] my-[4vh] py-4 px-6 rounded-e-3xl flex flex-row place-content-start items-center">
-		<img src="/space_helmet.webp" alt="" height="128" width="128" class="mx-6">
+	<div class="bg-lighttext mr-[10vw] my-[4vh] py-4 px-6 rounded-e-3xl flex md:flex-row flex-col place-content-start items-center">
+		<img src="/space_helmet.webp" alt="" height="128" width="128" class="md:mx-6 md:my-0 mb-6">
 		<p class="max-w-4xl m-auto">
 			I hold myself to very high standards, and know that my work benefits from that immensely. Any project I set my sights on gets that same quality treatment, regardless of if it's my own creation or one I was hired to work on.
 			<br>
