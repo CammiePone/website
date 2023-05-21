9e8bab3eddcb358b123acc1957c8f4eb85d995ac
CammiePone <inazuma261@gmail.com>
2023-05-21 01:05:31 -0400

    fixed on apple devices now hopefully haha h-full why do you do this

--- src/components/Socials.svelte
+++ src/components/Socials.svelte
@@ -3,7 +3,7 @@
 	const iconSize = 48;
 </script>
 
-<ul id="socials" class="z-10 h-full w-full grid gap-4 grid-flow-col auto-cols-max place-content-center mt-0 mb-1">
+<ul id="socials" class="z-10 w-full grid gap-4 grid-flow-col auto-cols-max place-content-center mt-0 mb-1">
 	<li class="m-auto inline-flex">
 		<a href="https://cammiescorner.dev/linkedin" target="_blank" rel="noopener noreferrer" class="inline-flex border-solid border-secondary border-4 bg-accent hover:rounded-xl focus:rounded-xl social-item-anchor">
 			<SocialIcons network="linkedin" fgColor="#2e3157" bgColor="#ffef91" width={iconSize} height={iconSize} />
