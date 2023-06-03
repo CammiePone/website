9774c2eca37ca59e3ebfcfde59ce51499b0dc31d
CammiePone <inazuma261@gmail.com>
2023-06-03 15:40:03 -0400

    some more mobile-only formatting

--- src/components/MonthSnip.svelte
+++ src/components/MonthSnip.svelte
@@ -4,10 +4,10 @@
 	let year = date.getFullYear();
 </script>
 
-<div class="bg-lighttext my-[4vh] py-4 px-6 flex md:flex-row flex-col [&:nth-child(odd)]:rounded-e-3xl [&:nth-child(even)]:rounded-s-3xl [&:nth-child(odd)]:mr-[10vw] [&:nth-child(even)]:ml-[10vw] md:[&:nth-child(even)]:flex-row-reverse [&:nth-child(even)]:flex-col-reverse">
-	<div class="flex flex-col md:[writing-mode:_vertical-lr] md:[text-orientation:_upright] max-h-fit my-4">
-		<!-- replace with date -->
+<div class="bg-lighttext my-[4vh] py-4 px-6 flex md:flex-row flex-col [&:nth-child(odd)]:rounded-e-3xl [&:nth-child(even)]:rounded-s-3xl [&:nth-child(odd)]:mr-[10vw] [&:nth-child(even)]:ml-[10vw] md:[&:nth-child(even)]:flex-row-reverse">
+	<div class="flex md:flex-col flex-row md:text-left text-center md:[writing-mode:_vertical-lr] md:[text-orientation:_upright] max-h-fit my-4">
 		<p class="uppercase">{month}</p>
+		<div class="md:px-0 px-1"></div>
 		<p>{year}</p>
 	</div>
 
