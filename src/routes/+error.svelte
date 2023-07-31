3e3eb433cb94564816ccbdc59eb6c21b58cd9555
CammiePone <inazuma261@gmail.com>
2023-07-31 13:28:16 -0700

    started on rss

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,5 +6,5 @@
 
 <div class="text-darktext text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-accent bg-opacity-75 p-3 rounded-3xl">
 	<h1 class="text-4xl font-bold">{$page.status}: {$page.error.message}</h1>
-	<p class="underline text-lg">my cat is sitting on my keyboard :&#40;</p>
+	<p class="text-lg">my cat is sitting on my keyboard :&#40;</p>
 </div>
--- src/routes/rss.ts
+++ src/routes/rss.ts
@@ -18,7 +18,7 @@ export const get: RequestHandler = async () => {
 const render = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 	<channel>
-		<atom:link href="http://cammiescorner.dev/blog/rss" rel="self" type="application/rss+xml" />
+		<atom:link href="http://cammiescorner.dev/rss" rel="self" type="application/rss+xml" />
 		<title>Cammie's Corner</title>
 		<link>https://cammiescorner.dev</link>
 		<description>Cammie's Blog</description>
