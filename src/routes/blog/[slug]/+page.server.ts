98621d0325acca2dc37b47638f8b735cbc688f88
CammiePone <inazuma261@gmail.com>
2023-08-14 14:17:24 -0700

    please work

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,7 +1,7 @@
 export function entries() {
-	const files = import.meta.glob("/src/blog-posts/*.md", {as: "raw", eager: false});
+	const files = import.meta.glob("/src/blog-posts/**/*.md", {as: "raw", eager: false});
 
-	return Object.keys(files).map((filename) => { return {slug: filename.substring("/src/blog-posts/".length, filename.length - 3)} });
+	return Object.keys(files).map((filename) => { return {slug: filename.substring(filename.lastIndexOf('/') + 1, filename.length - 3)} });
 }
 
 export const prerender = true;
